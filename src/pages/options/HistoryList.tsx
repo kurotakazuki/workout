37ab56147fafe1e3a23296306e360261ff8a6d87
kurotakazuki <kabisakanakabi@gmail.com>
2022-12-17 15:55:55 +0900

    wip

---
+++ src/pages/options/HistoryList.tsx
@@ -0,0 +1,8 @@
+import { createSignal, createMemo } from 'solid'
+import '@src/models/sites.ts'
+
+const HistoryList = () => {
+  return <div>list</div>
+}
+
+export default HistoryList
--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -1,7 +1,54 @@
+import { createSignal } from 'solid-js'
+import '@src/models/sites.ts'
+import HistoryList from './HistoryList'
+
 const Options = () => {
+  const [getBlockSiteUrl, setBlockSiteUrl] = createSignal<string>('')
+  const handleChange = (event: Event): void => {
+    if (!(event.target instanceof HTMLInputElement)) {
+      return
+    }
+    setBlockSiteUrl(event.target.value as string)
+  }
+  const addBlockSite = () => {
+    // const siteUrl = getBlockSiteUrl()
+    // if (siteUrl != '') {
+    //   chrome.storage.local.get('blockedSites', ({ blockedSites: Sites[] }) => {
+    //     blockedSites.push(siteUrl)
+    //     chrome.storage.local.set({ blockedSites })
+    //   })
+    // }
+  }
+
   return (
-    <div class="text-blue-600">
-      <div>Hello</div>
+    <div>
+      <div>
+        <form>
+          <p>ブロック時間：</p>
+          <p>
+            ブロックサイトの追加:
+            <input
+              type="text"
+              id="new-block-site"
+              size="60"
+              onChange={handleChange}
+            />
+          </p>
+          <button type="submit" onClick={addBlockSite}>
+            保存
+          </button>
+        </form>
+      </div>
+      <div>
+        <h2>ブロックリスト</h2>
+        <button type="submit" id="remove">
+          削除
+        </button>
+      </div>
+      m
+      <div>
+        <HistoryList />
+      </div>
     </div>
   )
 }
