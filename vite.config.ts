3f85c3cfd871d57ca05dc354f97fb0ff979f4658
kurotakazuki <kabisakanakabi@gmail.com>
2022-12-17 14:27:33 +0900

    add: eslint and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import solidPlugin from "vite-plugin-solid";
-import { crx } from "@crxjs/vite-plugin";
-import manifest from "./manifest.json";
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
 
 export default defineConfig({
   plugins: [solidPlugin(), crx({ manifest })],
-});
+})
