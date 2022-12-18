14036e6e24dfedadacbc100f64741f9b12f7b854
kuro <93798838+sp1st@users.noreply.github.com>
2022-12-18 10:55:12 +0900

    add: deleteBlockSite

--- src/pages/options/SiteList.tsx
+++ src/pages/options/SiteList.tsx
@@ -1,7 +1,23 @@
 import { Site } from '@src/models/sites'
 import { For } from 'solid-js'
+import { setBlockedSites } from './Options'
 
 const SiteList = (props: { sites: Site[] }) => {
+  const deleteBlockSite = (site: Site) => {
+      chrome.storage.local.get(
+        'blockedSites',
+        ({ blockedSites }: { blockedSites: Site[] }) => {
+          blockedSites = blockedSites.filter((currSite) => {
+            if (currSite !== site) {
+              return currSite
+            }
+          })
+        }
+      )
+      chrome.storage.local.set({ blockedSites })
+      setBlockedSites(blockedSites)
+  }
+  
   return (
     <div>
       <table class="table table-auto">
@@ -19,6 +35,12 @@ const SiteList = (props: { sites: Site[] }) => {
                 <td>{i}</td>
                 <td>{site.name}</td>
                 <td>{site.url}</td>
+                <button
+                  class="p-2 bg-blue-600 text-white"
+                  onClick={()=>deleteBlockSite(site)}
+                >
+                  削除
+                </button>
               </tr>
             )}
           </For>
