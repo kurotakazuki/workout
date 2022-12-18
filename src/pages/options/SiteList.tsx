3b5bb63322c37e5bbb2cc0b1febc89d9019f7d17
kuro <93798838+sp1st@users.noreply.github.com>
2022-12-18 16:24:08 +0900

    add: slide

--- src/pages/options/SiteList.tsx
+++ src/pages/options/SiteList.tsx
@@ -32,7 +32,7 @@ const SiteList = (props: { sites: Site[] }) => {
             <th scope="col" class="px-4 py-3">
               URL
             </th>
-            <th scope="col" class="px-4 py-3">
+            <th scope="col" class="px-2 py-3">
               削除
             </th>
           </tr>
